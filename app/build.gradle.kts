de6dcc7eb2bf6ff43223087c2fc70233a4c5ecaf
ChetanKumar Makwna <chetanm6907@gmail.com>
2026-09-14 06:44:03 +0530

    build: migrate to kotlin compiler options
    Update JVM target configuration to use the modern compiler options DSL.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,9 @@ android {
 }
 
 kotlin {
-  jvmToolchain(17)
+  compilerOptions {
+    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+  }
 }
 
 // Configure the Secrets Gradle Plugin to use .env and .env.example files
